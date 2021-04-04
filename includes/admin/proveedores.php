fbfe801ea1534f2c5bef6c25b76bd6ce311e31c4
Adnan Ebrahimi <adnan.ebrahimi@gmail.com>
2021-04-04 09:55:00 +0430

    linting

--- includes/admin/proveedores.php
+++ includes/admin/proveedores.php
@@ -226,16 +226,16 @@ function apg_sms_envia_sms( $apg_sms_settings, $telefono, $mensaje ) {
 			$respuesta					= wp_remote_post( "https://api.smscountry.com/SMSCwebservice_bulk.aspx", $argumentos );
 			break;
 		case "smsbox":
-				$url						= add_query_arg( [
-					'username'					=> $apg_sms_settings[ 'usuario_smsbox' ],
-					'password'					=> $apg_sms_settings[ 'contrasena_smsbox' ],
-					'customerId'				=> $apg_sms_settings[ 'customerid_smsbox' ],
-					'senderText'				=> $apg_sms_settings[ 'sendername_smsbox' ],
-					'recipientNumbers'			=> str_replace('','+',$telefono),
-					'messageBody'				=> apg_sms_codifica_el_mensaje( $mensaje ),
-					'isBlink'			=> 'false',
-					'isFlash'			=> 'false',
-					'defDate'			=> ' '
+				$url					= add_query_arg( [
+					'username'			    => $apg_sms_settings[ 'usuario_smsbox' ],
+					'password'			    => $apg_sms_settings[ 'contrasena_smsbox' ],
+					'customerId'			=> $apg_sms_settings[ 'customerid_smsbox' ],
+					'senderText'			=> $apg_sms_settings[ 'sendername_smsbox' ],
+					'recipientNumbers'		=> str_replace('','+',$telefono),
+					'messageBody'			=> apg_sms_codifica_el_mensaje( $mensaje ),
+					'isBlink'		    	=> 'false',
+					'isFlash'			    => 'false',
+					'defDate'		    	=> ' '
 				], 'https://www.smsbox.com/smsgateway/services/messaging.asmx/Http_SendSMS' );
 				$respuesta					= wp_remote_get( $url );
 				var_dump($respuesta);
